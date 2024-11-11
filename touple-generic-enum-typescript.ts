c22782ce9e97ca55e67b46a12aebddd83a5ba6cd
Imtiaj Sajin <imtiajsajin@gmail.com>
2024-11-11 16:28:23 +0600

    Create touple-generic-enum-typescript.ts

---
+++ touple-generic-enum-typescript.ts
@@ -0,0 +1,20 @@
+let a:[number,string,boolean]=[88,"Bangladesh",true] //touples
+console.log(a);
+
+
+
+//enum 
+enum eren { //enum
+    armin,
+    zeke,
+    jean=49,
+    bortheldo
+  }
+  
+  console.log(eren.zeke);
+  console.log(eren.bortheldo);
+
+
+  //generic
+  let generic:Array<number>=[ 45,46]
+  console.log(generic);
